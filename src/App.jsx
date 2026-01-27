15720a3108c488748d56a3bac2bdc665d4e82266
naitik-cmd <naitikchs16@gmail.com>
2026-01-28 01:10:51 +0530

    MacWindow created, using react-rnd.

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,15 @@ import React from 'react'
 import './app.scss'
 import Dock from './components/Dock'
 import Nav from './components/Nav'
+import MacWindow from './windows/MacWindow'
 const App = () => {
   return (
     <main>
       <Dock/>
       <Nav/>
+      <MacWindow>
+        <h1>Hello, Mac OS!</h1>
+      </MacWindow>
     </main>
   )
 }
